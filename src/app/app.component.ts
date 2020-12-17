ed72ed5f5b12f624bee49229c1a62855da190cc8
paolo.carraro <paolo.carraro@ekr.it>
2020-12-18 00:41:24 +0100

    add and example for get post delete put patch

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,7 +1,7 @@
 import { HttpClient } from '@angular/common/http';
 import { Component } from '@angular/core';
-import { title } from 'process';
-import { filter, map, mergeAll, take, takeLast, toArray } from 'rxjs/operators';
+import { filter, map, mergeAll, take, takeLast, toArray, catchError } from 'rxjs/operators';
+import { Todo } from './todo.model';
 
 @Component({
   selector: 'app-root',
@@ -10,18 +10,21 @@ import { filter, map, mergeAll, take, takeLast, toArray } from 'rxjs/operators';
 })
 export class AppComponent {
   result: string = "no data";
+  msgError: string = "";
+  showError: boolean = false;
 
   constructor(private delegate: HttpClient) { }
 
-  submitCall() {
+  retriveAndFilter() {
+    this.showError = false;
     //retrive collection of data
-    this.delegate.get("http://jsonplaceholder.typicode.com/todos")
+    this.delegate.get<Todo[]>("http://jsonplaceholder.typicode.com/todos")
       .pipe(
         //flat array
         mergeAll(),
         //take only last 10
         takeLast(10),
-        //get only 2 fields and transfrom it abd return new object
+        //get only 2 fields, transfrom it in uppercase and return new object
         map(({ title, completed }) => {
           return { title: String(title).toUpperCase(), completed: completed }
         }),
@@ -31,10 +34,68 @@ export class AppComponent {
         toArray(),
       )
       .subscribe(
-        data => this.result = JSON.stringify(data, null, 4)
+        data => this.result = JSON.stringify(data, null, 4),
       );
   }
 
+  postTodo() {
+    this.showError = false;
+    this.delegate.post<Todo>("http://jsonplaceholder.typicode.com/todos",
+      {
+        title: 'Hello world',
+        completed: false
+      }
+    )
+      .subscribe(
+        result => this.result = JSON.stringify(result, null, 4),
+        err => {
+          this.showError = true;
+          this.msgError = err.message;
+        }
+      );
+  }
+
+  patchTodo() {
+    this.showError = false;
+    this.delegate.patch<Todo>("http://jsonplaceholder.typicode.com/todos/2",
+      {
+        title: "hello patch"
+      }
+    )
+      .subscribe(
+        result => this.result = JSON.stringify(result, null, 4),
+        err => {
+          this.showError = true;
+          this.msgError = err.message;
+        }
+      );
+  }
+
+  putTodo() {
+    this.showError = false;
+    this.delegate.put<Todo>("http://jsonplaceholder.typicode.com/todos/2",
+      new Todo(2, 100, "ciao", true)
+    )
+      .subscribe(
+        result => this.result = JSON.stringify(result, null, 4),
+        err => {
+          this.showError = true;
+          this.msgError = err.message;
+        }
+      );
+  }
+
+  deleteTodo() {
+    this.showError = false;
+    this.delegate.delete("http://jsonplaceholder.typicode.com/todos/1")
+      .subscribe(
+        result => this.result = JSON.stringify(result, null, 4),
+        err => {
+          this.showError = true;
+          this.msgError = err.message;
+        }
+      )
+  }
 }
 
 
---
+++ src/app/todo.model.ts
@@ -0,0 +1,14 @@
+export class Todo {
+
+	id: number;
+	userId?: number;
+	title: string;
+	completed: boolean;
+
+	constructor(id: number, userId: number = -1, title: string, completed: boolean) {
+		this.id = id;
+		this.userId = userId;
+		this.title = title;
+		this.completed = completed;
+	}
+}
